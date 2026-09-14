8de4753a826e17352d4e4630d1d2d60fb7e1e0d4
DEBANJAN HAZRA <hazradev01@gmail.com>
2026-09-14 17:58:28 +0530

    Time: 55 ms (81.96%), Space: 21.2 MB (70.02%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxi=float("-inf")
+        total=0
+        for i in range(0,len(nums)):
+            total=total+nums[i]
+            maxi=max(maxi,total)
+            if total<0:
+                total=0
+        return maxi
+        
